fe825abc7a756d2351a9aad9de6d64a16f2a2976
fahrirafa <fahrrafa70@gmail.com>
2026-08-18 11:53:00 +0700

    LKM pohon komponen

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /* 
-      Antarmuka dibuat bertingkat (induk–anak) agar posisi dan tata letak widget 
-      bisa teratur dengan rapi, di mana widget anak mengikuti aturan dari induknya. 
-      Jika semua bagian ditulis sejajar tanpa hierarki, Flutter tidak akan tahu 
-      bagaimana membagi ruang layar, sehingga tampilan akan saling bertumpuk 
-      dan kodenya menjadi sangat berantakan.
-    */
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Koperasi Sekolah')),
