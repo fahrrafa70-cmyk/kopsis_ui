91c23f75a72ad06e204720645231a23a2ca3fd88
fahrirafa <fahrrafa70@gmail.com>
2026-08-20 13:42:23 +0700

    LKM-3 kartu barang

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
+int stok = 40;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,22 +11,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Koperasi Sekolah')),
-        body: Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Buku Tulis'),
-              const SizedBox(height: 8,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text('Anggota: Rp.3.000'),
-                  Text('Utama: Rp.3.500'),
-                ],
-              )
-            ],
-          )
+        body: Card(
+          // Jarak luar agar kartu tidak menempel ke tepi layar ponsel
+          margin: const EdgeInsets.all(12),
+          child: ListTile(
+            leading: const Icon(Icons.inventory_2),
+            title: const Text(
+              'Buku Tulis',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: const Text('Anggota Rp3.000 | Umum Rp3.500'),
+            trailing: Text(
+              'Stok: ' + stok.toString(),
+              style: TextStyle(color: stok == 0 ? Colors.red : Colors.black)
+              ),
+          ),
         ),
       ),
     );
